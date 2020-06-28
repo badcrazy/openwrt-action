c8a7d09f9de6d2e04a16ce4ac9276996df8aeeaa
badcrazy <badcrazy@gmail.com>
2020-06-28 23:24:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,4 @@ git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
 git clone https://github.com/jefferymvp/luci-app-koolproxyR.git package/lean/luci-app-koolproxyR
 git clone https://github.com/badcrazy/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 git clone https://github.com/Lienol/openwrt-OpenAppFilter.git package/lean/openwrt-OpenAppFilter
-git clone https://github.com/esirplayground/Lienol-obsolete.git package/lean/luci-app-passwall
 git clone https://github.com/ledewrt/luci-app-eqos.git package/lean/luci-app-eqos
