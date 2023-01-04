f53d2a79ec2da65583ddba9e04ac1a9e16d8043a
badcrazy <badcrazy@gmail.com>
2023-01-04 21:10:46 +0800

    Add files via upload

---
+++ axdiy-part2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+
+# Modify version
+sed -i 's/R22.11.11/V22.11.11/g' package/addition/default-settings/files/99-default-settings
+
